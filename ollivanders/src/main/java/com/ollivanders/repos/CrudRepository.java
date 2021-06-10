7445a149bcac199cdce39a3c18de2c5077ea1def
Kyle <kylea.castillo@calbaptist.edu>
2021-06-10 14:31:00 -0500

    Added more methods to CrudReposistory

--- ollivanders/src/main/java/com/ollivanders/repos/CrudRepository.java
+++ ollivanders/src/main/java/com/ollivanders/repos/CrudRepository.java
@@ -24,11 +24,16 @@ public interface CrudRepository<T> {
 	void removeClassTable(T tableObj, boolean cascade) throws NoSuchFieldException, SQLException;
 	
 	/**
+	 * Saves a specified object.
+	 * @param newObj
+	 */
+	void saveNewToClassTable(T newObj);
+	
+	/**
 	 * Saves a specified object into a specified table
 	 * @param newObj is the object to be saved
 	 * @param tableObj is the table of where its being saved
 	 */
-	
 	void saveNewToClassTable(T newObj, T tableObj);
 	
 	/**
@@ -38,4 +43,43 @@ public interface CrudRepository<T> {
 	 */
 	
 	T findByPrimaryKey(Object primaryKey) throws NoSuchFieldException, SQLException;
+	
+	/**
+	 * Finds an etry by its primary key from a table
+	 * @param primaryKey is the key used to search an entry for.
+	 * @param table is the table to be searched through.
+	 * @return returns the object with the corresponding id.
+	 */
+	
+	T findByPrimaryKey(Object primaryKey, Object table) throws NoSuchFieldException, SQLException;
+	
+	/**
+	 * Updates the given object in the database
+	 * @param updatedObj this is the object that needs to be updated.
+	 * @return returns true if changed, false if anything else happens.
+	 */
+	boolean updateByPrimaryKey(T updatedObj);
+	
+	/**
+	 * Updates the given object in the database within a table.
+	 * @param updatedObj this is the object that needs to be updated.
+	 * @param tableObj this is the table where the object is located.
+	 * @return returns true if changed, false if anything else happens.
+	 */
+	boolean updateByPrimaryKey(T updatedObj, T tableObj);
+	
+	/**
+	 * Removes the object from the database based on the primaryKey
+	 * @param primaryKey the key used to locate the entry from the database
+	 * @return returns true if the entry is deleted, false if anything else occurs.
+	 */
+	boolean deleteByPrimaryKey(Object primaryKey) throws NoSuchFieldException, SQLException;
+	
+	/**
+	 * Removes the object from the database based on the primaryKey and table.
+	 * @param primaryKey the key used to locate the entry from the database
+	 * @param tableObj
+	 * @return true if the entry is deleted, false if anything else occurs.
+	 */
+	boolean deleteByPrimaryKey(Object primaryKey, T tableObj) throws NoSuchFieldException, SQLException;
 }
