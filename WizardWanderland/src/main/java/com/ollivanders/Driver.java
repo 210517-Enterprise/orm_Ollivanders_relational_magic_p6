3ca532902a5318d27e031800722abc2b25dc6f27
Jake Geiser <jgeiser@uncc.edu>
2021-06-21 18:23:59 -0400

    added driver and models

---
+++ WizardWanderland/src/main/java/com/ollivanders/Driver.java
@@ -0,0 +1,10 @@
+package com.ollivanders;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WizardWanderland/src/main/java/com/ollivanders/model/Ingredient.java
@@ -0,0 +1,88 @@
+package com.ollivanders.model;
+
+public class Ingredient {
+	
+	public int id;
+	public String type;
+	public String name;
+	public double cost;
+	
+	public Ingredient() {
+		super();
+	}
+	public Ingredient(int id, String type, String name, double cost) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.name = name;
+		this.cost = cost;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingredient other = (Ingredient) obj;
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", type=" + type + ", name=" + name + ", cost=" + cost + "]";
+	}
+	
+	
+	
+}
